46ede9d3f9709da864c2bf396a53d414e71de7fb
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-13 10:33:02 +0100

    feat: project initialisation

---
+++ src/main/java/cat/itacademy/sprint_5_task_1_webflux/Sprint5Task1WebfluxApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.sprint_5_task_1_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint5Task1WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint5Task1WebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/sprint_5_task_1_webflux/Sprint5Task1WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.sprint_5_task_1_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint5Task1WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
